228a8dc410ebf139d60f846c0868a19c0d633401
vermouthdky <1048180828@qq.com>
2019-04-25 19:02:52 +0800

    修改组件为iview

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -5,3 +5,4 @@ export default {
   name: "LoginView"
 };
 </script>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Element from 'element-ui'
+import Vue from "vue";
+import App from "./App.vue";
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
 
-Vue.use(Element)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
