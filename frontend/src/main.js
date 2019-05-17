24635d41d108b00ecc1c45ad5159f987a1dec442
vermouthdky <1048180828@qq.com>
2019-05-17 12:14:59 +0800

    alter the index and box

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css'
+
 Vue.use(iView);
+Vue.use(SuiVue);
 
 Vue.config.productionTip = false;
 
