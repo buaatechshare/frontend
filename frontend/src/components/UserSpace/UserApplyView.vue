d72643872627537692bf5944c0aeb719e13084da
Skyvot <skyvot@buaa.edu.cn>
2019-05-25 19:10:32 +0800

    finished basic framework of UserIndex part. Still have some problems.

---
+++ frontend/src/components/UserSpace/UserApplyView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui container horizontal">
+    <div class="ui horizontal divider"></div>
+    <Input v-model="name" style="width: 400px">
+      <span slot="prepend">Name:</span>
+    </Input>
+    <br>
+    <Input v-model="email" style="width: 400px">
+      <span slot="prepend">Email:</span>
+    </Input>
+    <br>
+    <p>申请理由</p>
+    <Input v-model="reason" type="textarea" :rows="4" placeholder="nothing to show here"/>
+    <p></p>
+    <Button type="primary">提交</Button>
+    <div class="ui horizontal divider"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "paperBoxList",
+  data() {
+    return {
+      name: "Skyvot",
+      email: "test@buaa.edu.cn",
+      reason: "nothing to show here."
+    };
+  }
+};
+</script>
--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -11,41 +11,70 @@ import PatentView from '../components/PatentView.vue';
 // import ProfBoxList from '../components/BOXList/ProfBoxList.vue'
 import register from '../components/SignupView.vue';
 import login from '../components/LoginView.vue';
+import UserCollectionPaperView from '../components/UserSpace/UserCollectionPaperView.vue'
+import UserCollectionPatentView from '../components/UserSpace/UserCollectionPatentView.vue'
+import UserSpaceView from '../components/UserSpace/UserSpaceView.vue'
+import UserApplyView from '../components/UserSpace/UserApplyView.vue'
 
-export default [{
-  path: '/',
-  component: defaultIndex,
-  children: [{
-    // 当 /user/:id/profile 匹配成功，
-    // UserProfile 会被渲染在 User 的 <router-view> 中
-    path: 'papers',
-    component: PaperBoxList,
-  },
+export default [
   {
-    // 当 /user/:id/posts 匹配成功
-    // UserPosts 会被渲染在 User 的 <router-view> 中
-    path: 'patents',
-    component: PatentBoxList,
-  },
-  {
-    path: 'register',
-    component: register,
-  },
-  {
-    path: 'login',
-    component: login,
-  }, {
-    path: '/user',
+    path: '/userspace',
     component: userIndex,
+    children:
+      [
+        {
+          path: '/userspace/UserCollectionPaperView',
+          component: UserCollectionPaperView
+        },
+        {
+          path: '/userspace/UserCollectionPatentView',
+          component: UserCollectionPatentView
+        },
+        {
+          path: '/userspace/UserSpaceView',
+          component: UserSpaceView
+        },
+        {
+          path: '/userspace/UserApplyView',
+          component: UserApplyView
+        },
+      ]
   },
   {
-    path: '/paperview',
-    component: PaperView,
-  },
-  {
-    path: '/patentview',
-    component: PatentView,
-  },
-  ],
-}
+    path: '/',
+    component: defaultIndex,
+    children: [{
+      // 当 /user/:id/profile 匹配成功，
+      // UserProfile 会被渲染在 User 的 <router-view> 中
+      path: 'papers',
+      component: PaperBoxList,
+    },
+    {
+      // 当 /user/:id/posts 匹配成功
+      // UserPosts 会被渲染在 User 的 <router-view> 中
+      path: 'patents',
+      component: PatentBoxList,
+    },
+    {
+      path: 'register',
+      component: register,
+    },
+    {
+      path: 'login',
+      component: login,
+    }, {
+      path: '/user',
+      component: userIndex,
+    },
+    {
+      path: '/paperview',
+      component: PaperView,
+    },
+    {
+      path: '/patentview',
+      component: PatentView,
+    },
+    ],
+
+  }
 ];
