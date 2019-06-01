72da8379a43f064969f04b4e73af86cd43a35d41
zyh1019179614 <1019179614@qq.com>
2019-06-01 17:37:43 +0800

    专利界面接口

--- frontend/src/components/PatentView.vue
+++ frontend/src/components/PatentView.vue
@@ -7,7 +7,7 @@
 }
 .tablelabel {
   color: grey;
-  font-size: 12px;
+  font-size: 13px;
   width: 100px;
 }
 .papercontent {
@@ -18,7 +18,7 @@
 }
 .tablecontent {
   color: grey;
-  font-size: 12px;
+  font-size: 13px;
   width: 350px;
 }
 .comment {
@@ -33,69 +33,71 @@
       <sui-container fluid>
         <br>
         <!--标题-->
-        <h2 style="color:#006ddb" is="sui-header">蛙人助推器</h2>
+        <h2 style="color:#006ddb" is="sui-header">{{patentdetail.title}}</h2>
         <!--来源 收藏 阅读量-->
-        <p style="color:grey;font-size:12px">
-          来自 维普
-          <Divider type="vertical"/>
-          <sui-icon name="heart outline"/>收藏
-          <Divider type="vertical" dashed/>阅读量 5
+        <p style="color:grey;font-size:16px">
+          <Icon v-if="iscollect==false" type="ios-heart-outline" size="22" @click="collectpaper"/>
+          <Icon
+            v-if="iscollect==true"
+            type="ios-heart"
+            size="22"
+            style="color:#c60000"
+            @click="collectpaper"
+          />收藏
         </p>
         <!--专利信息-->
         <sui-table-row>
           <sui-table-cell class="tablelabel">申请号：</sui-table-cell>
-          <sui-table-cell class="tablecontent">CN201721535512.3</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.applicationNo}}</sui-table-cell>
           <sui-table-cell class="tablelabel">申请日：</sui-table-cell>
-          <sui-table-cell class="tablecontent">2017-11-16</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.applicationDate}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">公开号：</sui-table-cell>
-          <sui-table-cell class="tablecontent">CN208439405U</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.publicNo}}</sui-table-cell>
           <sui-table-cell class="tablelabel">公开日：</sui-table-cell>
-          <sui-table-cell class="tablecontent">2019-01-29</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.publicDate}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">授权公开号：</sui-table-cell>
-          <sui-table-cell class="tablecontent">CN208439405U</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.grantNo}}</sui-table-cell>
           <sui-table-cell class="tablelabel">授权公开日：</sui-table-cell>
-          <sui-table-cell class="tablecontent">2019-01-29</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.grantDate}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">申请人：</sui-table-cell>
-          <sui-table-cell class="tablecontent">周守祥</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.appliers}}</sui-table-cell>
           <sui-table-cell class="tablelabel">申请地：</sui-table-cell>
-          <sui-table-cell class="tablecontent">402191 重庆市永川区朱沱镇四望山村凉水井ewewewew村民小组35号</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.address}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">发明人：</sui-table-cell>
-          <sui-table-cell class="tablecontent">周守祥</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.inventors}}</sui-table-cell>
           <sui-table-cell class="tablelabel">当前权利人：</sui-table-cell>
-          <sui-table-cell class="tablecontent">邬剑星</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.obligee}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">专利代理机构：</sui-table-cell>
-          <sui-table-cell class="tablecontent">重庆谢成律师事务所</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.agency}}</sui-table-cell>
           <sui-table-cell class="tablelabel">代理人：</sui-table-cell>
-          <sui-table-cell class="tablecontent">邬剑星</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.agent}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">主分类号：</sui-table-cell>
-          <sui-table-cell class="tablecontent">B63C11/02</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.mainClassification}}</sui-table-cell>
           <sui-table-cell class="tablelabel">分类号：</sui-table-cell>
-          <sui-table-cell class="tablecontent">B63C11/02</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.classificationNo}}</sui-table-cell>
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell class="tablelabel">优先权：</sui-table-cell>
-          <sui-table-cell class="tablecontent">重庆谢成律师事务所</sui-table-cell>
+          <sui-table-cell class="tablecontent">{{patentdetail.priority}}</sui-table-cell>
         </sui-table-row>
         <Divider dashed/>
         <div style="clear:both"></div>
         <!--摘要-->
         <div>
           <p class="label">摘要：</p>
-          <p
-            class="papercontent"
-          >通过对近年来有关慢性疲劳综合征文献的回顾，总结传统中医药对慢性疲劳综合征病因病机、辨证施治、针灸治疗、抗疲劳的单味中药研究概况。传统中医药近年来对慢性疲劳综合征的研究以整体观为指导同时针对个体特点，多从脏腑、气血津液论治，也可辨证施以针灸治疗，并通过实验研究总结出不少具有抗疲劳的单杀中药。</p>
+          <p class="papercontent">{{patentdetail.abstract}}</p>
           <br>
           <!--为了上下间距美观加入换行-->
         </div>
@@ -105,43 +107,14 @@
         <div>
           <p class="label">关键词：</p>
           <p class="papercontent">
-            <span>
-              <a>慢性疲劳综合征</a>
-            </span>，
-            <span>
-              <a>中医药</a>
-            </span>，
-            <span>
-              <a>辩证施治</a>
+            <span v-for="keyword in keywords" style="margin-right:5px">
+              <a>{{keyword}}</a>
             </span>
           </p>
         </div>
 
         <div style="clear:both"></div>
 
-        <!--三个按钮-->
-        <div style="clear:both"></div>
-        <div style="margin-top:20px;margin-bottom:20px">
-          <sui-button
-            icon="star outline"
-            style="background-color:#cae4ff;color:#2693ff;margin:0px 5px"
-          >收藏</sui-button>
-          <sui-button
-            icon="linkify"
-            style="background-color:#cae4ff;color:#2693ff;margin:0px 5px"
-          >引用</sui-button>
-          <sui-button icon="share" style="background-color:#cae4ff;color:#2693ff;margin:0px 5px">分享</sui-button>
-        </div>
-        <sui-divider hidden/>
-        <!--下载链接-->
-        <div style="width:865px">
-          <Card dis-hover>
-            <p slot="title" style="font-size:17px">全文下载</p>
-            <p v-for="download in url">{{download}}</p>
-          </Card>
-        </div>
-
-        <sui-divider hidden/>
         <!--评论列表-->
         <sui-comment-group style="width:865px">
           <h3 is="sui-header" style="width:865px" dividing>评论列表</h3>
@@ -205,6 +178,7 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "patentview",
   data() {
@@ -213,8 +187,9 @@ export default {
       open: false,
       iscollect: false,
       patentdetail: [],
-      url: [],
       comments: [],
+      appliers: [],
+      classificationNo: [],
       commentModel: {
         comment: "",
         rate: 0,
@@ -275,11 +250,11 @@ export default {
       ])
       .then(
         axios.spread((PD, CO) => {
+          console.log(PD);
           this.patentdetail = PD.data.patentDetail;
           this.keywords = this.patentdetail.keywords;
-          this.fos = this.patentdetail.fos;
-          this.references = this.patentdetail.references;
-          this.url = this.patentdetail.url;
+          this.appliers = this.patentdetail.appliers;
+          this.classificationNo = this.patentdetail.classificationNo;
           this.comments = CO.data.comments;
         })
       );
