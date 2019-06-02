746661d6bb78ec6425f3a284fa17943dd81176b0
zyh1019179614 <1019179614@qq.com>
2019-06-02 17:50:00 +0800

    登录注册验证finish

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -6,22 +6,22 @@
         <h2 style="margin:10px;color:#abcdef;text-align:center">
           <sui-icon name="braille"></sui-icon>techshare
         </h2>
-        <Form ref="formValidate" :model="loginModel" :rules="ruleValidate">
-          <FormItem label="email" prop="email">
-            <Input v-model="loginModel.username" placeholder="Enter your email"></Input>
+        <Form ref="loginModel" :model="loginModel" :rules="ruleValidate">
+          <FormItem label="邮箱" prop="username">
+            <Input v-model="loginModel.username"></Input>
           </FormItem>
-          <FormItem label="password" prop="password">
-            <Input v-model="loginModel.password" placeholder="Enter your password"></Input>
+          <FormItem label="密码" prop="password">
+            <Input type="password" v-model="loginModel.password"></Input>
           </FormItem>
           <FormItem>
             <ButtonGroup>
-              <Button type="primary" to="/papers">cancel</Button>
-              <Button to="/register" style="float:left">signup</Button>
+              <Button type="primary" to="/papers">取消</Button>
+              <Button to="/register" style="float:left">注册</Button>
             </ButtonGroup>
           </FormItem>
         </Form>
         <div slot="footer">
-          <Button type="primary" @click="handleSubmit()" long>login</Button>
+          <Button type="primary" @click="handleSubmit('loginModel')" long>登录</Button>
         </div>
       </Modal>
     </div>
@@ -38,29 +38,51 @@ export default {
         username: "",
         password: ""
       },
-      ruleValidate: {},
+      ruleValidate: {
+        username: [
+          {
+            required: true,
+            message: "Mailbox cannot be empty",
+            trigger: "blur"
+          },
+          { type: "email", message: "Incorrect email format", trigger: "blur" }
+        ],
+        password: [
+          {
+            required: true,
+            message: "Password cannot be empty",
+            trigger: "blur"
+          }
+        ]
+      },
       loginShow: true
     };
   },
   methods: {
-    handleSubmit: function() {
-      axios
-        .post("/login/", this.loginModel)
-        .then(res => {
-          console.log(res);
-          if (res.status == 200) {
-            this.$Message.info("登录成功！");
-            this.$router.push({
-              name: "user",
-              params: { userID: "123333" }
+    handleSubmit: function(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          axios
+            .post("/login/", this.loginModel)
+            .then(res => {
+              console.log(res);
+              if (res.status == 200) {
+                this.$Message.info("登录成功！");
+                this.$router.push({
+                  name: "user",
+                  params: { userID: "123333" }
+                });
+              } else {
+                this.$Message.info("登录账号或者密码错误！");
+              }
+            })
+            .catch(err => {
+              console.error(err);
             });
-          } else {
-            this.$Message.info("登录账号或者密码错误！");
-          }
-        })
-        .catch(err => {
-          console.error(err);
-        });
+        } else {
+          this.$Message.error("信息有误!");
+        }
+      });
     }
   }
 };
--- frontend/src/components/SignupView.vue
+++ frontend/src/components/SignupView.vue
@@ -7,31 +7,31 @@
         <h2 style="margin:10px;color:#abcdef;text-align:center">
           <sui-icon name="braille"></sui-icon>techshare
         </h2>
-        <Form :model="signupModel" :rules="signupRule">
-          <FormItem label="Name" prop="username">
+        <Form ref="signupModel" :model="signupModel" :rules="signupRule">
+          <FormItem label="姓名" prop="name">
             <Input v-model="signupModel.name"></Input>
           </FormItem>
-          <FormItem label="Email" prop="email">
+          <FormItem label="邮箱" prop="email">
             <Input v-model="signupModel.email"></Input>
           </FormItem>
-          <FormItem label="phone" prop="phone">
+          <FormItem label="电话" prop="phone">
             <Input v-model="signupModel.phone"></Input>
           </FormItem>
-          <FormItem label="password" prop="password">
-            <Input v-model="signupModel.password"></Input>
+          <FormItem label="密码" prop="password">
+            <Input type="password" v-model="signupModel.password"></Input>
           </FormItem>
-          <FormItem label="password*2" prop="passwordDBL">
-            <Input v-model="signupModel.passwordDBL"></Input>
+          <FormItem label="密码确认" prop="passwordDBL">
+            <Input type="password" v-model="signupModel.passwordDBL"></Input>
           </FormItem>
           <FormItem>
             <ButtonGroup>
-              <Button type="primary" to="/papers">cancel</Button>
-              <Button to="/login" style="float:left">login</Button>
+              <Button type="primary" to="/papers">取消</Button>
+              <Button to="/login" style="float:left">登录</Button>
             </ButtonGroup>
           </FormItem>
         </Form>
         <div slot="footer">
-          <Button type="primary" @click="handleRegister()" long>signup</Button>
+          <Button type="primary" @click="handleRegister('signupModel')" long>注册</Button>
         </div>
       </Modal>
     </div>
@@ -42,6 +42,26 @@ import axios from "axios";
 export default {
   name: "signupView",
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please enter your password"));
+      } else {
+        if (this.signupModel.passwordDBL !== "") {
+          // 对第二个密码框单独验证
+          this.$refs.signupModel.validateField("passwordDBL");
+        }
+        callback();
+      }
+    };
+    const validatePassCheck = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please enter your password again"));
+      } else if (value !== this.signupModel.password) {
+        callback(new Error("The two input passwords do not match!"));
+      } else {
+        callback();
+      }
+    };
     return {
       signupModel: {
         name: "",
@@ -49,26 +69,57 @@ export default {
         password: "",
         phone: ""
       },
-      signupRule: {},
+      signupRule: {
+        name: [
+          {
+            required: true,
+            message: "The name cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: "Mailbox cannot be empty",
+            trigger: "blur"
+          },
+          { type: "email", message: "Incorrect email format", trigger: "blur" }
+        ],
+        phone: [
+          {
+            required: true,
+            message: "The phone cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        password: [{ validator: validatePass, trigger: "blur" }],
+        passwordDBL: [{ validator: validatePassCheck, trigger: "blur" }]
+      },
       signupShow: true
     };
   },
   methods: {
-    handleRegister: function() {
-      axios
-        .post("/userinfo/", this.signupModel)
-        .then(res => {
-          console.log(res);
-          if (res.status == 201) {
-            this.$router.push({
-              name: "registerfinish",
-              params: { userID: res.data.userID }
+    handleRegister: function(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          axios
+            .post("/userinfo/", this.signupModel)
+            .then(res => {
+              console.log(res);
+              if (res.status == 201) {
+                this.$router.push({
+                  name: "registerfinish",
+                  params: { userID: res.data.userID }
+                });
+              }
+            })
+            .catch(err => {
+              console.error(err);
             });
-          }
-        })
-        .catch(err => {
-          console.error(err);
-        });
+        } else {
+          this.$Message.error("信息有误!");
+        }
+      });
     }
   }
 };
